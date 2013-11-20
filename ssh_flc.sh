7b504989799c6eb7e1cc05a257b490092aefdc59
Tal <tal@server.fake>
2013-11-19 20:28:16 -0700

    Removed while loop. Cron should run the script

--- ssh_flc.sh
+++ ssh_flc.sh
@@ -5,32 +5,29 @@
 #that we've already read
 CHECKED_LOGS=/home/test/checked.log
 
-while [[ true ]]
+#Figure out how many different source IP addresses failed to login
+#in the past 2 minutes (NOT 120 seconds! - more like in the current minute and the
+#previous minute)
+FAILED_IPs=$(grep --color=auto -i 'sshd' /var/log/auth.log | grep -i 'failed password' | grep --color=auto "$(date +'%b %d %R')\|$(date --date='-1 minute' +'%b %d %R')" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+
+IFS=$'\n'
+for IP in $(echo "$FAILED_IPs" | sort -u)
 do
-	#Figure out how many different source IP addresses failed to login
-	#in the past 2 minutes (NOT 120 seconds! - more like in the current minute and the
-	#previous minute)
-	FAILED_IPs=$(grep --color=auto -i 'sshd' /var/log/auth.log | grep -i 'failed password' | grep --color=auto "$(date +'%b %d %R')\|$(date --date='-1 minute' +'%b %d %R')" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+	#See how many failed logins the IP we are looking at had
+	#in the past 2 minutes
+	if [[ $(echo "$FAILED_IPs" | grep -c "^${IP}$") -ge 10 ]]
+	then
+		#$IP has made over 10 failed attempts to login in the past 10 minutes
 
-	IFS=$'\n'
-	for IP in $(echo "$FAILED_IPs" | sort -u)
-	do
-		#See how many failed logins the IP we are looking at had
-		#in the past 2 minutes
-		if [[ $(echo "$FAILED_IPs" | grep -c "^${IP}$") -ge 10 ]]
+		#Make sure $IP is not already blocked by iptables
+		if ! iptables -nL INPUT | grep "$IP[^0-9]" | grep -q REJECT
 		then
-			#$IP has made over 10 failed attempts to login in the past 10 minutes
+			iptables -A INPUT -s $IP -p tcp --dport 22 -j REJECT
 
-			#Make sure $IP is not already blocked by iptables
-			if ! iptables -nL INPUT | grep "$IP[^0-9]" | grep -q REJECT
-			then
-				iptables -A INPUT -s $IP -p tcp --dport 22 -j REJECT
-
-				#Start a new thread that will unblock the IP after x seconds
-				( sleep 300; iptables -D INPUT -s $IP -p tcp --dport 22 -j REJECT ) & 
-			fi
+			#Start a new thread that will unblock the IP after x seconds
+			( sleep 300; iptables -D INPUT -s $IP -p tcp --dport 22 -j REJECT ) & 
 		fi
-	done
-
-	exit 0
+	fi
 done
+
+exit 0
